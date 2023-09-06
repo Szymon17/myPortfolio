3e114ab6bb704426f01044b4576ff421b3c7b5e2
Szymon17 <jablonski.szymon21@gmail.com>
2023-09-06 22:32:45 +0200

    finished timeline

--- src/components/time-line/time-line.jsx
+++ src/components/time-line/time-line.jsx
@@ -3,6 +3,29 @@ import { motion } from "framer-motion";
 import DATA from "./TIME-LINE_DATA.json";
 import "./time-line.css";
 
+const animations = {
+  list: {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.2,
+        staggerChildren: 0.3,
+      },
+    },
+  },
+
+  item: {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 0.5, staggerChildren: 0.3 } },
+  },
+
+  text: {
+    hidden: { opacity: 0, y: -300 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  },
+};
+
 const TimeLine = () => {
   const { items } = DATA;
   const timelineRef = useRef();
@@ -12,31 +35,41 @@ const TimeLine = () => {
   const [offset, setOffset] = useState(0);
   const [dragFieldWidth, setDragField] = useState(0);
   const [itemWidth, setItemWidth] = useState(300);
+  const [padding, setPadding] = useState(30);
   const [distanceBetweenItems, setItemDistance] = useState(50);
 
   useEffect(() => {
     const changeItemsSizeToFitPage = () => {
-      let tmpItemWidth = itemWidth;
-      let tmpDistanceBetweenItems = distanceBetweenItems;
-
-      let itemsRatio = (timelineRef.current.offsetWidth - 40) / (tmpItemWidth + tmpDistanceBetweenItems);
-
+      let tmpItemWidth = 300;
+      let tmpDistanceBetweenItems = 50;
       let maxLoopCount = 100;
 
-      if (itemsRatio - Math.floor(itemsRatio) >= 0.5) {
-        while (itemsRatio - Math.floor(itemsRatio) >= 0.5 && itemsRatio - Math.floor(itemsRatio) < 0.9 && maxLoopCount > 0) {
-          tmpItemWidth -= 10;
-          tmpDistanceBetweenItems -= 1;
-          itemsRatio = (timelineRef.current.offsetWidth - 40) / (tmpItemWidth + tmpDistanceBetweenItems);
-          maxLoopCount--;
+      const calculateItemsRation = () => (timelineRef.current.offsetWidth - padding) / (tmpItemWidth + tmpDistanceBetweenItems);
+
+      let itemsRatio = calculateItemsRation();
+      console.log(itemsRatio);
+      if (timelineRef.current.offsetWidth > 350) {
+        if (itemsRatio - Math.floor(itemsRatio) >= 0.6) {
+          while (itemsRatio - Math.floor(itemsRatio) >= 0.6 && itemsRatio - Math.floor(itemsRatio) < 0.9 && maxLoopCount > 0) {
+            tmpItemWidth -= 5;
+            tmpDistanceBetweenItems -= 6;
+            itemsRatio = calculateItemsRation();
+            maxLoopCount--;
+          }
+        } else {
+          while (itemsRatio - Math.floor(itemsRatio) < 0.6 && maxLoopCount > 0) {
+            tmpItemWidth += 5;
+            tmpDistanceBetweenItems += 6;
+            itemsRatio = calculateItemsRation();
+            maxLoopCount--;
+
+            console.log("wykonano");
+          }
         }
+        setPadding(30);
       } else {
-        while (itemsRatio - Math.floor(itemsRatio) < 0.5 && maxLoopCount > 0) {
-          tmpItemWidth += 10;
-          tmpDistanceBetweenItems += 1;
-          itemsRatio = (timelineRef.current.offsetWidth - 20) / (tmpItemWidth + tmpDistanceBetweenItems);
-          maxLoopCount--;
-        }
+        tmpItemWidth = timelineRef.current.offsetWidth - 20;
+        setPadding(10);
       }
 
       setItemWidth(tmpItemWidth);
@@ -59,43 +92,43 @@ const TimeLine = () => {
   useEffect(() => {
     const changeDragSize = () => {
       const { width } = timelineRef.current.getBoundingClientRect();
-
       const offSetWidth = contentRef.current.scrollWidth;
       const newOffset = offSetWidth - width;
 
+      console.log(newOffset, offSetWidth);
       setOffset(newOffset);
       setDragField(offSetWidth);
-      console.log("cześć wykonałem zmianę offsetu na ", newOffset, offSetWidth, width);
     };
     changeDragSize();
+
+    console.log(padding);
   }, [itemWidth]);
 
   return (
-    <section ref={timelineRef} className="timeLine shadow">
+    <motion.section style={{ padding: padding }} initial="hidden" whileInView="show" ref={timelineRef} className="timeLine shadow">
       <div
         ref={dragFieldRef}
         className="timeLine__dragField"
         style={{
-          left: `-${offset}px`,
+          left: `-${offset + padding}px`,
           width: `${dragFieldWidth}px`,
         }}
       ></div>
-      {offset}
       <motion.div key={offset} ref={contentRef} dragConstraints={dragFieldRef} drag="x" className="timeLine__container">
-        <div style={{ width: (items.length - 1) * (itemWidth + distanceBetweenItems) }} className="timeLine__line" />
-        <ul className="timeLine__list">
+        <motion.ul variants={animations.list} className="timeLine__list">
           {items.map(({ date, text }, index) => (
-            <li style={{ left: (itemWidth + distanceBetweenItems) * index }} key={index} className="timeLine__item">
-              <div className="timeLine__circle" />
-              <div className="timeLine__textContainer">
+            <motion.li variants={animations.item} style={{ left: (itemWidth + distanceBetweenItems) * index }} key={index} className="timeLine__item">
+              <motion.div variants={animations.text} style={{ width: itemWidth }} className="timeLine__textContainer">
                 <h4 className="timeLine__title">{date}</h4>
                 <p className="timeLine__text">{text}</p>
-              </div>
-            </li>
+              </motion.div>
+              <div className="timeLine__circle" />
+            </motion.li>
           ))}
-        </ul>
+        </motion.ul>
+        <div style={{ width: (items.length - 1) * (itemWidth + distanceBetweenItems) }} className="timeLine__line" />
       </motion.div>
-    </section>
+    </motion.section>
   );
 };
 
